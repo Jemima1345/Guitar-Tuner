8ac8b47959a94c76128581612571a5eafe882b8a
Jemima1345 <jemima1345@hotmail.com>
2022-07-28 05:29:15 -0600

    find closest note

--- tuner.c
+++ tuner.c
@@ -10,15 +10,23 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#define NUM_SAMPLE_PERIODS  1
+#define NUM_SAMPLE_PERIODS  10
 #define TIM1_CLK_FREQ       1000000 // 8MHZ/8 / 1
+#define NOTES_ARRAY_LEN     49
 
-uint16_t measure_frequency(void);
 uint16_t get_timer_count(void);
+uint16_t measure_frequency(void);
+uint16_t find_closest_note(uint16_t);
 
 /*
  * 
  */
+const uint16_t notes_array[] = {27.5,29.14,30.87,32.7,34.65,36.71,38.89,41.2,43.65,
+                       46.25,49,51.91,55,58.27,61.74,65.41,69.3,73.42,77.78,
+                       82.41,87.31,92.5,98,103.83,110,116.54,123.47,130.81,
+                       138.59,146.83,155.56,164.81,174.61,185,196,207.65,220,
+                       233.08,246.94,261.63,277.18,293.66,311.13,329.63,349.23,
+                       369.99,392,415.3,440}; //TODO: change to float
 
 volatile uint8_t num_overflows = 0;
 
@@ -51,11 +59,13 @@ int main(int argc, char** argv) {
     /* enable global interrupts */
     sei();
     
-    uint16_t note = measure_frequency();
+    uint16_t plucked_note, closest_note;
     
     
     while(1){
-        note = measure_frequency();
+        
+        plucked_note = measure_frequency();
+        closest_note = find_closest_note(plucked_note);
     }
     
     return (EXIT_SUCCESS);
@@ -78,6 +88,21 @@ ISR(TIMER1_OVF_vect){
 //ISR(TIMER1_CAPT_vect){
 //    /* automatically clears ICF1 */
 //}
+
+uint16_t get_timer_count(void){
+    
+    unsigned char sreg;
+    uint16_t tim1_count;
+
+    /* Save global interrupt flag */
+    sreg = SREG;
+    /* disable global interrupts before reading from ICR1 */
+    cli();
+    tim1_count = ICR1;
+    /* Restore global interrupt flag */
+    SREG = sreg;
+    return tim1_count;
+}
 
 uint16_t measure_frequency(void){
     uint16_t t_start, t_end, note_freq;
@@ -106,17 +131,32 @@ uint16_t measure_frequency(void){
 
 }
 
-uint16_t get_timer_count(void){
+uint16_t find_closest_note(uint16_t plucked_note){
     
-    unsigned char sreg;
-    uint16_t tim1_count;
-
-    /* Save global interrupt flag */
-    sreg = SREG;
-    /* disable global interrupts before reading from ICR1 */
-    cli();
-    tim1_count = ICR1;
-    /* Restore global interrupt flag */
-    SREG = sreg;
-    return tim1_count;
+    uint16_t top_note, bottom_note, closest_note;
+    uint8_t dist_to_top, dist_to_bottom;
+    
+    top_note = bottom_note = dist_to_top = dist_to_bottom = closest_note = 0;
+    
+    for(uint8_t note_index = 0; note_index < NOTES_ARRAY_LEN; note_index++){
+        
+        if(notes_array[note_index] > plucked_note){
+            top_note = notes_array[note_index];
+            bottom_note = notes_array[note_index-1];
+            
+            dist_to_top = top_note - plucked_note;
+            dist_to_bottom = plucked_note - bottom_note;
+            
+            if(dist_to_bottom < dist_to_top){
+                closest_note = bottom_note;
+            }else {
+                /* if the plucked note is the same distance from the two nearest
+                 * notes in the array, use the higher note as the closest */
+                closest_note = top_note;
+            }     
+            return closest_note;
+        }
+    }
+    // note played is higher than the max note in the array
+    return notes_array[NOTES_ARRAY_LEN-1]; 
 }
